b67255a0c6152788a02d04750180f525b1aca305
Ben Firth <bd_firth@hotmail.com>
2017-04-18 16:36:23 -0500

    Created skeleton Android UI

--- AndroidMessenger/Resources/Resource.Designer.cs
+++ AndroidMessenger/Resources/Resource.Designer.cs
@@ -60,32 +60,35 @@ namespace AndroidMessenger
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050003
-			public const int ConnectDevice = 2131034115;
-			
 			// aapt resource value: 0x7f050001
-			public const int ConversationOutput = 2131034113;
-			
-			// aapt resource value: 0x7f050008
-			public const int DeviceList = 2131034120;
-			
-			// aapt resource value: 0x7f050007
-			public const int Disconnect = 2131034119;
-			
-			// aapt resource value: 0x7f050000
-			public const int GetConversations = 2131034112;
+			public const int Connect = 2131034113;
 			
 			// aapt resource value: 0x7f050005
-			public const int MessageContent = 2131034117;
+			public const int ConnectDevice = 2131034117;
 			
-			// aapt resource value: 0x7f050004
-			public const int PhoneNumber = 2131034116;
+			// aapt resource value: 0x7f050009
+			public const int DeviceList = 2131034121;
+			
+			// aapt resource value: 0x7f050003
+			public const int Disconnect = 2131034115;
+			
+			// aapt resource value: 0x7f050007
+			public const int MessageContent = 2131034119;
 			
 			// aapt resource value: 0x7f050006
-			public const int Send = 2131034118;
+			public const int PhoneNumber = 2131034118;
 			
 			// aapt resource value: 0x7f050002
-			public const int SentObject = 2131034114;
+			public const int SelectDevice = 2131034114;
+			
+			// aapt resource value: 0x7f050008
+			public const int Send = 2131034120;
+			
+			// aapt resource value: 0x7f050004
+			public const int SentObject = 2131034116;
+			
+			// aapt resource value: 0x7f050000
+			public const int Status = 2131034112;
 			
 			static Id()
 			{
